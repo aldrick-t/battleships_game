5c46c3ab0c85ee5859d899d248399b25069f79f4
Aldrick T <taldrick@gmail.com>
2024-06-13 11:14:28 -0600

    Fixed issues with attack display system.

--- battleships_ino/battleships_ino_pio/src/battleships_ino.cpp
+++ battleships_ino/battleships_ino_pio/src/battleships_ino.cpp
@@ -27,21 +27,27 @@ int tableroataqueaa[10][3]; // Coordenada y intensidad
 int shipCount = 0;
 int x = 0, y = 0;
 
-unsigned long lastDebounceTime[5] = {0}; 
+unsigned long lastDebounceTime[6] = {0}; 
 unsigned long debounceDelay = 50; 
 bool gameStarted = false; // Variable para controlar si el juego ha comenzado o no
 bool shipsPlaced = false; // Variable para controlar si los barcos han sido colocados
-bool viewMode = false; // Variable para controlar si se está en modo de visualización
+bool viewMode = false; // false for ship view, true for attack view
+int lastAttack[2] = {-1, -1}; // Último ataque recibido
 
 // Function declarations
 void placeShips();
 void receiveAttack();
 void displayCursorSHIPS(int x, int y);
 void receiveShipCoordinates();
+void switchView();
+void displayShipView();
+void displayAttackView();
+void moveCursorForAttack();
+bool readButton(int buttonPin, int buttonIndex);
 
 bool readButton(int buttonPin, int buttonIndex) {
-  static bool lastButtonState[5] = {HIGH, HIGH, HIGH, HIGH, HIGH};
-  static bool buttonState[5] = {HIGH, HIGH, HIGH, HIGH, HIGH};
+  static bool lastButtonState[6] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
+  static bool buttonState[6] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH};
   bool reading = digitalRead(buttonPin);
 
   if (reading != lastButtonState[buttonIndex]) {
@@ -61,64 +67,6 @@ bool readButton(int buttonPin, int buttonIndex) {
 
   lastButtonState[buttonIndex] = reading;
   return false;
-}
-
-void displayShipView() {
-  mx.clear();
-  for (int i = 0; i < 5; i++) {
-    if (tablerob[i][0] != -1) {
-      mx.setPoint(tablerob[i][1], tablerob[i][0], true);
-      mx.setPoint(tablerob[i][1], tablerob[i][0] + 1, true);
-    }
-  }
-}
-
-void displayAttackView() {
-  mx.clear();
-  for (int i = 0; i < 10; i++) {
-    if (tableroataqueaa[i][0] != -1) {
-      mx.setPoint(tableroataqueaa[i][1], tableroataqueaa[i][0], true);
-    }
-  }
-}
-
-void switchView() {
-  viewMode = !viewMode;
-  if (viewMode) {
-    // Display the attack view
-    displayAttackView();
-  } else {
-    // Display the ship view
-    displayShipView();
-  }
-}
-
-void moveCursorForAttack() {
-  if (readButton(BTN_RIGHT, 0)) {
-    if (x < 7) { x++; }
-  }
-  if (readButton(BTN_LEFT, 1)) {
-    if (x > 0) { x--; }
-  }
-  if (readButton(BTN_UP, 2)) {
-    if (y > 0) { y--; }
-  }
-  if (readButton(BTN_DOWN, 3)) {
-    if (y < 7) { y++; }
-  }
-  if (readButton(BTN_ENTER, 4)) {
-    // Send attack coordinates to Raspberry Pi
-    Serial.print("attack:");
-    Serial.print(x);
-    Serial.print(",");
-    Serial.println(y);
-    // Flash LED at attacked coordinate
-    for (int i = 0; i < 5; i++) {
-      mx.setPoint(y, x, i % 2 == 0);
-      delay(200);
-    }
-    mx.setPoint(y, x, false);  // Turn off LED after flashing
-  }
 }
 
 void setup() {
@@ -159,10 +107,48 @@ void loop() {
       placeShips(); // Continuar ubicando los barcos
     } else {
       if (viewMode) { // Attack mode
-        moveCursorForAttack();
+      moveCursorForAttack();
       } else {
         receiveAttack(); // Recibir ataques
       }
+    }
+  }
+}
+
+void switchView() {
+  viewMode = !viewMode;
+  if (viewMode) {
+    // Display the attack view
+    displayAttackView();
+  } else {
+    // Display the ship view
+    displayShipView();
+  }
+}
+
+void displayShipView() {
+  mx.clear();
+  for (int i = 0; i < 5; i++) {
+    if (tablerob[i][0] != -1) {
+      mx.setPoint(tablerob[i][1], tablerob[i][0], true);
+      mx.setPoint(tablerob[i][1], tablerob[i][0] + 1, true);
+    }
+  }
+  // Flash the latest attack received
+  if (lastAttack[0] != -1 && lastAttack[1] != -1) {
+    for (int i = 0; i < 5; i++) {
+      mx.setPoint(lastAttack[1], lastAttack[0], i % 2 == 0);
+      delay(100);
+    }
+    mx.setPoint(lastAttack[1], lastAttack[0], false);  // Turn off LED after flashing
+  }
+}
+
+void displayAttackView() {
+  mx.clear();
+  for (int i = 0; i < 10; i++) {
+    if (tableroataqueaa[i][0] != -1) {
+      mx.setPoint(tableroataqueaa[i][1], tableroataqueaa[i][0], true);
     }
   }
 }
@@ -188,27 +174,32 @@ void placeShips() {
   }
 
   if (readButton(BTN_ENTER, 4)) {
-    tablerob[shipCount][0] = x;
-    tablerob[shipCount][1] = y;
-    shipCount++;
-    mx.setPoint(y, x, true);
-    mx.setPoint(y, x + 1, true);  // Set the second part of the ship
+    if (shipCount < 5) {
+      tablerob[shipCount][0] = x;
+      tablerob[shipCount][1] = y;
+      shipCount++;
+      mx.setPoint(y, x, true);
+      mx.setPoint(y, x + 1, true);  // Set the second part of the ship
+      // Verificar si todos los barcos han sido colocados
+      if (shipCount == 5) {
+        shipsPlaced = true; // Cambiar el estado para indicar que los barcos han sido colocados
+    
+        // Send ship coordinates to Raspberry Pi
+        for (int i = 0; i < 5; i++) {
+          Serial.print(tablerob[i][0]);
+          Serial.print(",");
+          Serial.print(tablerob[i][1]);
+          Serial.print(",");
+          Serial.print(tablerob[i][0] + 1);
+          Serial.print(",");
+          Serial.print(tablerob[i][1]);
+          if (i < 4) Serial.print(" ");
+        }
+        Serial.println(" positions");  // Add " positions" to indicate all ships are placed
 
-    // Check if all ships have been placed
-    if (shipCount == 5) {
-      shipsPlaced = true; // Change the state to indicate ships have been placed
-      
-      // Send ship coordinates to Raspberry Pi
-      for (int i = 0; i < 5; i++) {
-        Serial.print(tablerob[i][0]);
-        Serial.print(",");
-        Serial.print(tablerob[i][1]);
-        if (i < 4) Serial.print(" ");
+        // Send confirmation to Raspberry Pi
+        Serial.println("ready");
       }
-      Serial.println(" positions");  // Add " positions" to indicate all ships are placed
-
-      // Send confirmation to Raspberry Pi
-      Serial.println("ready");
     }
   }
 }
@@ -225,18 +216,19 @@ void displayCursorSHIPS(int x, int y) {
   mx.setPoint(y, x + 1, true);  // Set the second part of the ship
 }
 
-
 void receiveShipCoordinates() {
   if (Serial.available()) {
     String data = Serial.readStringUntil('\n');
     int index = 0;
     int commaIndex = data.indexOf(',');
-    while (commaIndex != -1 && index < 5) {
-      tableroa[index][0] = data.substring(0, commaIndex).toInt();
-      int spaceIndex = data.indexOf(' ', commaIndex);
-      if (spaceIndex == -1) spaceIndex = data.length();
-      tableroa[index][1] = data.substring(commaIndex + 1, spaceIndex).toInt();
-      data = data.substring(spaceIndex + 1);
+    while (commaIndex != -1 && index < 10) {
+      int x = data.substring(0, commaIndex).toInt();
+      data = data.substring(commaIndex + 1);
+      commaIndex = data.indexOf(',');
+      int y = data.substring(0, commaIndex).toInt();
+      tableroa[index][0] = x;
+      tableroa[index][1] = y;
+      data = data.substring(commaIndex + 1);
       commaIndex = data.indexOf(',');
       index++;
     }
@@ -247,11 +239,9 @@ void receiveAttack() {
   if (Serial.available()) {
     String data = Serial.readStringUntil('\n');
     int commaIndex = data.indexOf(',');
-
     if (commaIndex != -1) {
       ataquea[0] = data.substring(0, commaIndex).toInt();
       ataquea[1] = data.substring(commaIndex + 1).toInt();
-
       bool hit = false;
       int hitIndex = -1;
       for (int i = 0; i < 5; i++) {
@@ -261,7 +251,6 @@ void receiveAttack() {
           break;
         }
       }
-
       // Flash LED at attacked coordinate
       for (int i = 0; i < 5; i++) {
         mx.setPoint(ataquea[1], ataquea[0], i % 2 == 0);
@@ -285,9 +274,41 @@ void receiveAttack() {
         }
       }
 
+      // Save the last attack received
+      lastAttack[0] = ataquea[0];
+      lastAttack[1] = ataquea[1];
+
       // Switch to attack mode after a delay
       delay(2000);
       switchView();
     }
+  }
+}
+
+void moveCursorForAttack() {
+  if (readButton(BTN_RIGHT, 0)) {
+    if (x < 7) { x++; }
+  }
+  if (readButton(BTN_LEFT, 1)) {
+    if (x > 0) { x--; }
+  }
+  if (readButton(BTN_UP, 2)) {
+    if (y > 0) { y--; }
+  }
+  if (readButton(BTN_DOWN, 3)) {
+    if (y < 7) { y++; }
+  }
+  if (readButton(BTN_ENTER, 4)) {
+    // Send attack coordinates to Raspberry Pi
+    Serial.print("attack:");
+    Serial.print(x);
+    Serial.print(",");
+    Serial.println(y);
+    // Flash LED at attacked coordinate
+    for (int i = 0; i < 5; i++) {
+      mx.setPoint(y, x, i % 2 == 0);
+      delay(100);
+    }
+    mx.setPoint(y, x, false);  // Turn off LED after flashing
   }
 }
